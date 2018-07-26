d0e9b5c78dbc1260cf4b67694e28cfe100f2ea77
StarScribe <41297732+StarScribe@users.noreply.github.com>
2018-07-26 10:00:05 +0300

    jsonrequest

--- app/src/main/java/dev/com/example/MainActivity.java
+++ app/src/main/java/dev/com/example/MainActivity.java
@@ -1,6 +1,7 @@
 package dev.com.example;
 
 import android.net.nsd.NsdManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,15 +11,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     Button button1;
     Button Login;
     EditText EmailView;
     EditText pass;
-
     EditText username;
+
 
 
     @Override
@@ -29,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Main2Activity.class));
-
+              //  startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                runrequest();
             }
         });
         EmailView = findViewById(R.id.EmailView);
@@ -46,5 +60,30 @@ public class MainActivity extends AppCompatActivity {
                                      }
                                  }
         );
+
 }
+public  void  runrequest (){
+    RequestQueue queue = Volley.newRequestQueue(this);
+    String url = "https://reqres.in/api/login";
+    
+    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+
+            (Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+
+
+                @Override
+                public void onResponse(JSONObject response) {
+                }
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+queue.add(jsonObjectRequest);
+
+            }
     }
+
+
